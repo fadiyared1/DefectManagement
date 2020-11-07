efd7c232b6417e7ecf06ae2b3eeeb10e0133fd64
michel <201710460@ua.edu.lb>
2020-11-07 13:44:35 +0200

    ss

--- database/migrations/2020_11_07_102119_create_roles_table.php
+++ database/migrations/2020_11_07_102119_create_roles_table.php
@@ -13,7 +13,7 @@ class CreateRolesTable extends Migration
      */
     public function up()
     {
-        Schema::create('role', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
             $table->string('role');
