200e1d30a9f4e9d3c0506ac54163906cc2e7f3bd
michel <201710460@ua.edu.lb>
2020-11-07 13:18:45 +0200

    g

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::DefaultStringLength(191);
+       // Schema::DefaultStringLength(191);
     }
 }
