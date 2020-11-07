e88ef668361311a7c2b66dc85ed595d042bcd664
michel <201710460@ua.edu.lb>
2020-11-07 15:35:08 +0200

    d

--- database/migrations/2020_11_07_103631_create_statuses_table.php
+++ database/migrations/2020_11_07_103631_create_statuses_table.php
@@ -14,7 +14,7 @@ class CreateStatusesTable extends Migration
     public function up()
     {
         Schema::create('status', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
             $table->string('status');
             $table->timestamps();
         });
--- database/migrations/2020_11_07_111404_create_defects_table.php
+++ database/migrations/2020_11_07_111404_create_defects_table.php
@@ -14,7 +14,7 @@ class CreateDefectsTable extends Migration
     public function up()
     {
         Schema::create('defect', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
             $table->string('title');
             $table->string('description');
             $table->string('expectedReason');
