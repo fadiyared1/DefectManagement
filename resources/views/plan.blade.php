d205bf774ecbd6c9b744ecc79359452f3f113834
michel <201710460@ua.edu.lb>
2020-11-20 23:48:41 +0200

    s

--- resources/views/plan.blade.php
+++ resources/views/plan.blade.php
@@ -1,11 +1,79 @@
 <html>
 <head>
+<style>
+h1 {
+  color: blue;
+  
+}
+#logo{
+    margin-left:1400px;
+    margin-top:0px;
+}
+#plan{
+    margin-left:350px;
+}
+#welcome{
+    margin-top:0px;
+}
+</style>
 <title>PLAN 
 </title>
 </head>
 <body>
-<h1>restaurant plan </h1>
+<div >
+<img src="E:\engineering\web programming 2\logo.jpg" alt="logo" width="100"
+         height="80" id="logo">
+         
+<h1 id="welcome">Welcome to Java shop </h1>
+<h1>
+Please report the problem you have
+</h1>
+</div>
+ <div>
+ <div class="form-group">
+                <label for="WC">WC<span class="WC"></span></label>
+                <select type="WC" class="WC" id="WC" name="WC">
+                    <option value="">Choose...</option>
+                    <option value="defect1"> defect1 </option>
+                    <option value="defect2">defect2 </option>
+                    <option value="defect3"> defect3 </option>
+                    <option value="defect4"> defect4 </option>
+                    
+                </select>
+<br>
+                <label for="tables">tables<span class="tables"></span></label>
+                <select type="tables" class="tables" id="tables" name="tables">
+                    <option value="">Choose...</option>
+                    <option value="defect1"> defect1 </option>
+                    <option value="defect2">defect2 </option>
+                    <option value="defect3"> defect3 </option>
+                    <option value="defect4"> defect4 </option>
+                    
+                </select>
+<br>
+                <label for="kitchen">kitchen<span class="kitchen"></span></label>
+                <select type="kitchen" class="kitchen" id="kitchen" name="kitchen">
+                    <option value="">Choose...</option>
+                    <option value="defect1"> defect1 </option>
+                    <option value="defect2">defect2 </option>
+                    <option value="defect3"> defect3 </option>
+                    <option value="defect4"> defect4 </option>
+                    
+                </select>
+                
+            </div>
 <img src="E:\engineering\web programming 2\plan5.png" alt="plan" width="800"
-         height="400">
+         height="570" id="plan">
+ </div>
+ <footer>
+ 
+  <p>contact us:</p>
+  <p>+96176010434</p>
+  <p>+96171700552</p>
+ 
+  <p><a href="mailto:201710460@ua.edu.lb">201710460@ua.edu.lbb</a></p>
+  <p><a href="mailto:201710493@ua.edu.lb">201710493@ua.edu.lbb</a></p>
+ 
+</footer>
 </body>
 </html>
